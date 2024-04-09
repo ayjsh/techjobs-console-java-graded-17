e945c3ea1a9ea686b30347d717f6a496045dae0b
Matt Stephens <mdstephens33@hotmail.com>
2024-04-08 20:09:57 -0500

    Completed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -35,7 +35,6 @@ public class TechJobs {
 
                 String columnChoice = getUserSelection("List", columnChoices);
 
-                assert columnChoice != null;
                 if (columnChoice.equals("all")) {
                     printJobs(JobData.findAll());
                 } else {
@@ -59,9 +58,8 @@ public class TechJobs {
                 System.out.println("\nSearch term:");
                 String searchTerm = in.nextLine();
 
-                assert searchField != null;
                 if (searchField.equals("all")) {
-                    printJobs(Objects.requireNonNull(JobData.findByValue(searchTerm)));
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -73,10 +71,10 @@ public class TechJobs {
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
         int choiceIdx = -1;
-        Boolean validChoice = false;
+        boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -118,7 +116,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (someJobs.size() == 0) {
+        if (someJobs.isEmpty()) {
             System.out.print("No Results");
         } else {
             for (int i = 0; i < someJobs.size(); i++) {
